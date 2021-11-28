d8f79453ef26de0c3495b52dc56ec37a5a60c5e1
TenaciousTapioca <89610605+TenaciousTapioca@users.noreply.github.com>
2021-11-27 23:03:35 -0500

    Comment banker.hpp

--- banker.hpp
+++ banker.hpp
@@ -1,7 +1,7 @@
-/**
+/** banker.hpp
  *  Nghia Dao
  *  Banker's Algorithm
- *  CS
+ *  CS33211
  */
 
 #ifndef BANKER_HPP
@@ -10,8 +10,8 @@
 #include <vector>
 #include <fstream>
 
-std::vector<std::vector<int>> getState(const std::string&);
-void getData(std::vector<int>&, std::ifstream&, char&);
-std::string getSafetySeq(const std::vector<std::vector<int>>&);
+std::vector<std::vector<int>> getState(const std::string&);     // obtains the state given by the text file
+void getData(std::vector<int>&, std::ifstream&, char&);         // parses text file so that the state is organized
+std::string getSafetySeq(const std::vector<std::vector<int>>&); // determines safe sequence if possible
 
 #endif
